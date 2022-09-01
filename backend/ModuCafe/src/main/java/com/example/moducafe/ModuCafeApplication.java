6eddc38bc1651706aaf21334c348968899d809d9
tear94fall <tear94fall@naver.com>
2022-09-01 18:57:21 +0900

    Feat: "스프링 백엔드 프로젝트 추가"
    스프링 프로젝트 추가
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

---
+++ backend/ModuCafe/src/main/java/com/example/moducafe/ModuCafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.moducafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuCafeApplication.class, args);
+    }
+
+}
---
+++ backend/ModuCafe/src/test/java/com/example/moducafe/ModuCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moducafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuCafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
