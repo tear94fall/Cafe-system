f5bd5b17b41f82690ac7e663be6974d8d84f39ba
tear94fall <tear94fall@naver.com>
2022-09-13 15:56:59 +0900

    Feat: "안드로이드 스플래시 액티비티 추가"
    안드로이드 스플래시 액티비티 추가
    - 모두의 카페 로고 표시 되도록 추가
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

---
+++ android/ModuCafe/app/src/main/java/com/example/moducafe/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.moducafe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        setTitle("로그인");
+    }
+}
