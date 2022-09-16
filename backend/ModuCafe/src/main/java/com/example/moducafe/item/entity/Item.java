f731229d0acda09c92240cc7d4b9a25ebe2315b9
tear94fall <tear94fall@naver.com>
2022-09-16 20:14:29 +0900

    Feat: "BaseTimeEntity 추가"
    백엔드 BaseTimeEntity 추가
    application.yaml 파일에 JPA 설정 추가
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

--- backend/ModuCafe/src/main/java/com/example/moducafe/item/entity/Item.java
+++ backend/ModuCafe/src/main/java/com/example/moducafe/item/entity/Item.java
@@ -1,5 +1,6 @@
 package com.example.moducafe.item.entity;
 
+import com.example.moducafe.common.entity.BaseTimeEntity;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-public abstract class Item {
+public abstract class Item extends BaseTimeEntity {
     @Id
     @GeneratedValue
     @Column(name = "item_id")
