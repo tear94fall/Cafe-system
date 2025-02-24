f618c0721b4fc4b1127143202a1b99aa106d5695
tear94fall <tear94fall@naver.com>
2025-02-24 23:18:03 +0900

    Feat: "자바 서비스 삭제"
    자바 서비스 삭제
    - 코틀린 서비스 추가를 위한 자바 서비스 삭제
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

--- backend/ModuCafe/src/main/java/com/example/moducafe/item/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.moducafe.item.repository;
-
-import com.example.moducafe.item.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Long>, ItemCustomRepository {
-}
